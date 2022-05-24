11aa0018c8a235b57e79a86cd8b67827166cb7a0
CharanTeja <charanteja.n@HOFINCONS.DOMAIN>
2022-05-24 17:33:29 +0530

    updates

--- src/components/Master.js
+++ src/components/Master.js
@@ -5,7 +5,9 @@ import { OverlayPanel } from 'primereact/overlaypanel'
 
 const Master = () => {
     
-    const [desks , setDesks] = useState([]) 
+    const [desks , setDesks] = useState([])
+    const [users , setusers] = useState([])
+    const [workspaceData , setWorkspaceData] = useState([])
     const  deskDetails = useRef(null)
 
    useEffect(()=>{
@@ -17,6 +19,20 @@ const Master = () => {
             setDesks(loadedDesks)
         }
 
+        const allWorkspace = localStorage.getItem("workspace")
+        const loadedWorkspace = JSON.parse(allWorkspace)
+
+        if(loadedWorkspace){
+            setWorkspaceData(loadedWorkspace)
+        }
+
+        const allUsers = localStorage.getItem("users")
+        const loadedUsers = JSON.parse(allUsers)
+
+        if(loadedUsers){
+            setusers(loadedUsers)
+        }
+
    },[])
 
     return(
@@ -26,16 +42,39 @@ const Master = () => {
                <div className="col-10">
                    <div className="grid">
                         {
-                           desks.map(items =><div key={items.chairNumber} className="col-1" style={{width:"10%"}}> 
-                                                    <Button icon="pi pi-user"  className="p-button-text mr-2 p-button-warning p-button-rounded p-button-sm" onClick={(e) => deskDetails.current.toggle(e)}/>    
-                                                </div>
-                               
-                           )
-                        }
-                        <OverlayPanel ref={deskDetails} showCloseIcon id="overlay_panel">
-                            <div>Hello</div>
-                        </OverlayPanel>
+                           desks.map(items =>{
+
+                                return (
+                                        <div key={items.id} className="col-1" style={{width:"10%"}} > 
+                                            <Button icon="pi pi-user" className="p-button-text mr-2 p-button-warning p-button-rounded p-button-sm" onClick={(e)=>deskDetails.current.toggle(e)}/>
+                                        </div>
+                                        )
+                            })
+                        
+                        }   
                    </div>
+                   <OverlayPanel ref={deskDetails} showCloseIcon id="overlay_panel">
+                        <div className="grid">
+                            <div className="col-6">
+                                Chair Number
+                            </div>
+                            <div className="col-6">
+                                {workspaceData.chairNumber}
+                            </div>
+                            <div className="col-6">
+                                User
+                            </div>
+                            <div className="col-6">
+                                {workspaceData.userName}
+                            </div>
+                            <div className="col-6">
+                                Expires On
+                            </div>
+                            <div className="col-6">
+                                {workspaceData.chairValidity}
+                            </div>
+                        </div>
+                    </OverlayPanel>
                </div>
                <div className="col-1"></div>
             </div>
--- src/components/workspace/WorkspaceContainer.js
+++ src/components/workspace/WorkspaceContainer.js
@@ -3,7 +3,7 @@ import Workspace from './Workspace'
 import {Button} from 'primereact/button'
 import {Dialog} from 'primereact/dialog'
 import {Card} from 'primereact/card' 
-import AssignChairForm from './AssingChairForm'
+import AssignChairForm from './AssignChairForm'
 import  {v4 as uuidv4} from 'uuid'
 
 const WokrspaceContainer = (props) => {
